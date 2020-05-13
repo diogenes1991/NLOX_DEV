82fd134c9b7b56a58e018cae84cec16ef9d1d245
Diogenes Figueroa <diogenes1991@gmail.com>
2020-05-13 19:22:57 -0400

    Update to add a LaTeX output layer

--- Python_Code/matrices.py
+++ Python_Code/matrices.py
@@ -1,121 +1,5 @@
 import sys
-
-class Particle:    
-    
-    def __init__(self,Name,Color,Charge,HyperCharge,Spin,Mass):
-        self.nam = Name
-        self.col = Color
-        self.chg = Charge
-        self.hyp = HyperCharge
-        self.mas = Mass
-        self.spn = Spin
-        
-    def __str__(self):
-        return self.nam
-    
-    def __eq__(self,oth):
-        if(self.nam == oth.nam and self.col == oth.col and self.chg == oth.chg and self.hyp == oth.hyp and self.mas == oth.mas and self.spn == oth.spn):
-            return True
-        else:
-            return False
-        
-    def Cross_Symmetry(Particle):
-        self.chg = -self.chg
-
-
-class CompositeParticle:
-    
-    def __init__(self,Name,Subparticles):
-        self.sub = Subparticles
-        self.nam = Name
-    
-    def __str__(self):
-        return self.nam
-
-class Process:           
-
-    def __init__(self,Initial_State,Final_State):
-        if(len(Initial_State)!=2):
-            print "Error: The initial state must have 2 particles."
-            sys.exit()
-        else:
-            self.ini = Initial_State
-        if(len(Final_State)<2):
-            print "Error: The final state must have at least 2 particles"
-            sys.exit()
-        else:
-            self.fin = Final_State
-        
-        self.proc = []
-        for i in self.ini:
-            self.proc.append(i)
-        for i in self.fin:
-            self.proc.append(i)
-        
-    def __str__(self):
-        aux = ""
-        for i in self.ini:
-            aux += i.nam +" "
-        aux += " --> "
-        for i in self.fin:
-            aux += i.nam + " "
-        return aux
-    
-    #def SubProcesses(self,SubProc):
-        #RedProc = []
-        ##print self.proc
-        #if (len(self.proc)>1):
-            #for i in xrange(1,len(self.proc)):
-                ##print self.proc[i].nam
-                #RedProc.append(self.proc[i])
-        #else 
-            #RedProc = self.proc
-        
-        #SubProcesses(RedProc,RedProc)
-        
-        #for i in self.proc[1].sub:
-                #for j in RedProc:
-                ##print i.nam
-                    #SubProc.append(i)
-                
-            
-            
-
-
-b = Particle("b",1,-1,1/2,1/2,True)
-s = Particle("s",1,-1,1/2,1/2,False)
-d = Particle("d",1,-1,1/2,1/2,False)
-u = Particle("u",1,2,1/2,1/2,False)
-c = Particle("c",1,2,1/2,1/2,False)
-g = Particle("g",1,0,0,1,False)
-
-#proton = CompositeParticle("p",[g,d,u,s,c,b])
-
-#jet = CompositeParticle("j",[g,d,u,s,c])
-
-
-#MyProc=Process([proton,proton],[jet,jet,jet])
-
-s = []
-
-#MyProc.SubProcesses(s)
-
-#print MyProc
-
-#a = {"Name":"Juan","Age":14,"Dict":{"Hello":"World"}}
-#print a
-#a["List"] = [1,2,3,4,5]
-#a["W"] = 11
-#a["AAA"] = 33
-#del a["List"]
-#g = "Age"
-#if g in a.keys():
-    #del a[g]
-#for i in a.keys():
-    #print "The key is:",i,"while the value is:",a[i]
-#h = a.keys()
-#print h
-
+from math import *
 
 def PRINT(MATRIX):
     for i in MATRIX:
@@ -216,6 +100,35 @@ def crear(Vertices,Aristas):
     return matriz
 
 
+
+def create_latex(n):
+    
+    # Ring graph 
+    
+    f  = open("Graph.tex","w")
+    l  = ''
+    l += '\\begin{center}\n'
+    l += '\\begin{tikzpicture}[>=stealth\',shorten >=0.5pt,auto,node distance=1.5cm,semithick]\n'
+    l += '\\tikzstyle{every state}=[fill=red,draw=red,minimum size=3mm]\n'
+    
+    for i in range(n):
+        l += '\\node[state] ('+str(i)+') at ('+str(cos(2*pi*i/n))+','+str(sin(2*pi*i/n))+') [] {};\n'
+         
+    l += '\\draw[red]\n'
+    for i in range(n):
+        l += '('+str((i)%n)+') edge  node {} ('+str((i+1)%n)+')\n'
+    l += ';\n'
+    
+    l += '\\end{tikzpicture}\n'
+    l += '\\end{center}\n'
+    f.write(l)
+    
+    
+    
+    
+    
+    
+
     
 #  
 #  La cantidad de circuitos cerrados de tamano n 
@@ -259,4 +172,5 @@ PRINT(F)
 print 'The matrix E.D is'
 PRINT(G)
 
-    
+create_latex(7)
+
