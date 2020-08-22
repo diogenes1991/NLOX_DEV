e1bbf1d5f4fa32d6e4b507f663cb5de9587a68b4
Diogenes Figueroa <diogenes1991@gmail.com>
2020-08-22 15:45:13 -0400

    Pochammer Symbols methods forward and backwards

--- Sum_Solver/Discrete_Calculus.h
+++ Sum_Solver/Discrete_Calculus.h
@@ -1,5 +1,6 @@
 
 f PochammerSymbol;
+f Summmation;
 
 auto s n;
 
@@ -15,9 +16,6 @@ auto s n;
 
 #endprocedure
 
-
-
-
 #procedure ToPochammer(EXPR,VAR)
 
     .sort 
@@ -30,32 +28,70 @@ auto s n;
 
     Local EX = replace_(`VAR',nTPCH)*(`EXPR');
     $xmax = -1;
+    $cache = (replace_(`VAR',0)*(`EXPR'))*PochammerSymbol(nTPCH,0);
+    $den = 1;
     if ( count(nTPCH,1) > $xmax ) $xmax = count_(nTPCH,1);
     .sort
+    Local EX = 0;
+    
     #message The maximum term has degree = `$xmax'
     
-    
+    #do i=1,`$xmax'
+        
+        #call FiniteDifference(`EXPR',`VAR')
+        $cache = $cache + (`$den')*(replace_(`VAR',0)*(`EXPR'))*PochammerSymbol(nTPCH,`i');
+        $den = `$den'*({`i'+1})^(-1);
+        .sort
+        
+    #enddo
+        
+    `EXPR' = replace_(nTPCH,`VAR')*(`$cache');
+    .sort
 
 
 #endprocedure
 
 
-
-
-
-
-
-
-#procedure SumPochammer($EXPR,$VAR)
+#procedure EvalPochammer(EXPR,VAR)
 
     .sort 
+
+    Local EX = replace_(`VAR',nEPCH)*(`EXPR');
+
+    repeat;
+    id PochammerSymbol(nEPCH,0) = 1;
+    id PochammerSymbol(nEPCH,n?) = (nEPCH+n-1)*PochammerSymbol(nEPCH,n-1);
+    endrepeat;
+    
+    id nEPCH = `VAR';
+    
+    .sort
+    Local EX = 0;
+    `EXPR' = EX;
+    .sort
+
+#endprocedure
+
+
+
+#procedure Sum(EXPR,VAR)
+
 *
 *  This procedure is based on a simple 
 *  premisse, any polynomial function in 
 *  the summation variable can be exactly 
 *  summed in terms of Pochammer Symbols
 *
+    #call ToPochammer(`EXPR',`VAR')
     
-    Local EXPR = `EXPR'
-
+    Local EX = replace_(`VAR',nSUMM)*(`EXPR');
+    
+    id PochammerSymbol(nSUMM,n?) = (n+1)^(-1)*PochammerSymbol(nSUMM,n+1);
+    
+    .sort 
+    
+    `EXPR' = replace_(nSUMM,`VAR')*(EX);
+    
+    #call EvalPochammer(`EXPR',`VAR')
+    
 #endprocedure
